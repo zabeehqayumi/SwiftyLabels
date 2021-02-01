e0f43b46d26fd23626d85661aeef7678d4c6351c
Zabeehullah Qayumi <zabeeh.qayumi@gmail.com>
2021-01-31 19:19:36 -0800

    Added test cases

---
+++ SwiftyLabels/SwiftyCoolLabels.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftyCoolLabels.swift
+//  SwiftyLabels
+//
+//  Created by Zab on 1/31/21.
+//
+
+import Foundation
+
+public final class SwiftyLabels {
+    
+    let name = "SwiftyLabels"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+}
