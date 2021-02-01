45fab312aebd04eb5281a82997486f75750a25df
Zabeehullah Qayumi <zabeeh.qayumi@gmail.com>
2021-01-31 18:40:25 -0800

    Initial Commit

---
+++ SwiftyLabels/SwiftyLabels.h
@@ -0,0 +1,18 @@
+//
+//  SwiftyLabels.h
+//  SwiftyLabels
+//
+//  Created by Zab on 1/31/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyLabels.
+FOUNDATION_EXPORT double SwiftyLabelsVersionNumber;
+
+//! Project version string for SwiftyLabels.
+FOUNDATION_EXPORT const unsigned char SwiftyLabelsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyLabels/PublicHeader.h>
+
+
---
+++ SwiftyLabelsExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftyLabelsExamples
+//
+//  Created by Zab on 1/31/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
